eec1b5d4b496efc4ad35eff11377f1b88b4d7775
ffay <81072619@qq.com>
2017-04-18 22:25:52 +0800

    Update ProxyConfig.java

--- proxy-server/src/main/java/org/fengfei/lanproxy/server/config/ProxyConfig.java
+++ proxy-server/src/main/java/org/fengfei/lanproxy/server/config/ProxyConfig.java
@@ -36,7 +36,7 @@ public class ProxyConfig implements Serializable {
 
     static {
 
-        // 代理配置信息存放在用户更目录下
+        // 代理配置信息存放在用户根目录下
         String dataPath = System.getProperty("user.home") + "/" + ".lanproxy/";
         File file = new File(dataPath);
         if (!file.isDirectory()) {
