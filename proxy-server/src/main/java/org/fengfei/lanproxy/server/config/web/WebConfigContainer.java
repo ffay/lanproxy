2b43c685e954366252e425dc2c2b8ae0b3cfa0d2
ffay <81072619@qq.com>
2018-06-09 23:29:27 +0800

    Update WebConfigContainer.java

--- proxy-server/src/main/java/org/fengfei/lanproxy/server/config/web/WebConfigContainer.java
+++ proxy-server/src/main/java/org/fengfei/lanproxy/server/config/web/WebConfigContainer.java
@@ -43,7 +43,7 @@ public class WebConfigContainer implements Container {
                         ChannelPipeline pipeline = ch.pipeline();
 
                         pipeline.addLast(new HttpServerCodec());
-                        pipeline.addLast(new HttpObjectAggregator(64 * 1024));
+                        pipeline.addLast(new HttpObjectAggregator(8 * 1024 * 1024));
                         pipeline.addLast(new ChunkedWriteHandler());
                         pipeline.addLast(new HttpRequestHandler());
                     }
